3ca88ef48225de8ea7b5445175aad8944c5915a5
Aditya Kalyanakrishnan <aditya.kalyanakrishnan@knowlarity.com>
2015-08-28 20:13:25 +0530

    completed all page settings

--- pjfotographie/views/pages.py
+++ pjfotographie/views/pages.py
@@ -7,6 +7,8 @@ simple_page = Blueprint('simple_page', __name__, template_folder='templates')
 @simple_page.route('/<page>/')
 def show(page):
     try:
+        if page.find('my-art') != -1:
+            return render_template('my-art.html')
         return render_template('%s.html' % page)
     except TemplateNotFound:
         abort(404)
