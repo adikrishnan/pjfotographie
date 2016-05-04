e4dd20fe366093439098f7b8b410b9bc15b4dce2
Adi Krishnan <aditya.k2190@gmail.com>
2016-05-04 23:21:18 +0530

    sorting files for display

--- pjfotographie/utils/os_utils.py
+++ pjfotographie/utils/os_utils.py
@@ -15,4 +15,6 @@ def fetch_all_files(rel_path):
     file_path = os.path.abspath(__file__)
     dir_path = os.path.sep.join(file_path.split(os.path.sep)[:-2])
     final_path = '%s%s' % (dir_path, rel_path)
-    return os.listdir(final_path)
+    all_files = os.listdir(final_path)
+    all_files.sort()
+    return all_files
